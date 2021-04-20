b92d626eef09cc8cd40b9330a8ac8afdbbdc7786
irisida <20445648+irisida@users.noreply.github.com>
2021-04-20 19:28:26 +0100

    Initial Commit

---
+++ instaclone/instacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instacloneApp.swift
+//  instaclone
+//
+//  Created by ed on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct instacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
