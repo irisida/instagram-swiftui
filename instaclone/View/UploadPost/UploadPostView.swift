63a861030063b3a73d2e24e19f31e46de337f33d
irisida <20445648+irisida@users.noreply.github.com>
2021-04-24 14:39:29 +0100

    added create new post basic UI and some condition controller

--- instaclone/View/UploadPost/UploadPostView.swift
+++ instaclone/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
+    
     var body: some View {
-        Text("Upload Post")
+        VStack {
+            if postImage != nil {
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "camera.circle")
+                        .resizable()
+                        .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/)
+                        .scaledToFill()
+                        .font(Font.title.weight(.ultraLight))
+                        .frame(width: 180, height: 180)
+                        .foregroundColor(Color.black)
+                        .clipped()
+                        .padding(.top, 56)
+                })
+            } else {
+                HStack(alignment: .top) {
+                    Image("batman")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter the post caption", text: $captionText )
+                }.padding()
+                
+                Button(action: {}, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(5.0)
+                }).padding()
+            }
+            
+            Spacer()
+        }
     }
 }
 
